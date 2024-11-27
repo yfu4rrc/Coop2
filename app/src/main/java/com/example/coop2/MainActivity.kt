f563db29bf972041d4c92446eea1870afabda7bc
yfu4rrc <130174099+yfu4rrc@users.noreply.github.com>
2024-11-27 08:44:33 -0600

    Initial

---
+++ app/src/main/java/com/example/coop2/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.coop2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.coop2.ui.theme.Coop2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Coop2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
